e75b9c54aae88bcffc8461c7306dbf37a4c24d11
Fruncis <93474605+Fruncis@users.noreply.github.com>
2023-04-26 08:22:22 +0200

    Add files via upload

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ProgettoInformatica.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public RelayCommand ClickCommandEvent { get; set; }
+
+        public MainWindowViewModel()
+        {
+            ClickCommandEvent = new RelayCommand(ClickExecute);
+            
+        }
+
+        public void ClickExecute(object param)
+        {
+            System.Diagnostics.Debug.WriteLine($"Clicked: Cacca");
+        }
+
+        public string _name = "cracrapu";
+        public string Name 
+        { 
+            get 
+            {
+                return _name;
+            }
+            set 
+            {
+                _name = value;
+            } 
+        }
+    }
+}
