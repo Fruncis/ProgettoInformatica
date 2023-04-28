c2612d13161b433297a0218aadec5187bd65204d
Fruncis <93474605+Fruncis@users.noreply.github.com>
2023-04-28 12:31:50 +0200

    Add files via upload

---
+++ Model/Carta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoInformatica.Model
+{
+    public class Carta
+    {
+        private string _titolo;
+        public string Titolo { get; set; }
+        private string _quesito;
+        public string Quesito { get; set; }
+        private string[] _risposte;
+        public string[] Risposte { get; set; }
+        private string _ripostaCorretta;
+        public string RipostaCorretta { get; set; }
+
+
+        public Carta(string titolo, string quesito, string rispostaCorretta, string[] risposte)
+        {
+            _titolo = titolo;
+            _quesito = quesito;
+            _ripostaCorretta = rispostaCorretta;
+            _risposte = new string[4];
+            for(int i=0; i<4; i++)
+            {
+                _risposte[i] = risposte[i]; 
+            }
+        }
+    }
+}
---
+++ Model/Mazzo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace ProgettoInformatica.Model
+{
+    public class Mazzo
+    {
+        public string TipoMazzo { get; set; } = string.Empty;
+        private Carta[] carte;
+
+
+        public Mazzo()
+        {
+            //TipoMazzo = json;
+            carte = new Carta[20];
+        }
+
+        public List<Mazzo> UseFileReadAllTextWithSystemTextJson()
+        {
+            var _options = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            using FileStream streamReader = File.OpenRead("mazzo.json");
+            var json = streamReader.ReadToEnd();
+            List<Mazzo> list = JsonSerializer.Deserialize<List<Mazzo>>(json, _options);
+            return list;
+        }
+
+
+    }
+}
